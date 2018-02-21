5f8f09634e23f408dcb242b5f901d9e10183796b
Steven Torres <stevetorres7562@gmail.com>
2018-02-21 21:54:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+var recipes = {cereal: 'milk'}; 
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return  Object.assign({},object,{[key]:value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key]= value 
+  return object 
+}
+
+function deleteFromObjectByKey(object, key){
+  return delete object.key 
+  
+}
+
